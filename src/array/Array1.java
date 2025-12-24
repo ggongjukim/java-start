303dfeb406ec81cf7c12c566485a6be4f5b8433e
ggongjukim <kch7892003@gmail.com>
2025-12-24 15:20:15 +0900

    섹션 9. 배열 > Array

---
+++ src/array/Array1.java
@@ -0,0 +1,36 @@
+package array;
+
+import java.util.Scanner;
+
+public class Array1 {
+    public static void main(String[] args) {
+//        입력받을 숫자의 개수를 입력하세요:3 3개의 정수를 입력하세요:
+//        1
+//        2
+//        5
+//        가장 작은 정수: 1 가장 큰 정수: 5
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("입력받을 숫자의 개수를 입력하세요 : ");
+        int inputNum = scanner.nextInt();
+
+        int[] numbers = new int[inputNum];
+        numbers[0] = scanner.nextInt();
+        int maxNum = numbers[0];
+        int minNum = numbers[0];
+
+        for(int i = 1; i < inputNum; i++){
+            numbers[i] = scanner.nextInt();
+
+            if(numbers[i] > maxNum){
+                maxNum = numbers[i];
+            }
+            if(numbers[i] < minNum){
+                minNum = numbers[i];
+
+            }
+        }
+
+        System.out.println("가장 작은 정수: " + minNum + " 가장 큰 점수 :" + maxNum);
+    }
+}
